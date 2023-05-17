030f314b11d8e737b76b0cba2bb5a0dae44a55dd
Benjamin Laroche <benjamin.laroche28@gmail.com>
2023-05-18 00:04:37 +0200

    first commit

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,10 @@
 function PokemonCard() {
   return (
-    <figure>
+    <figure className="card">
       <img
         src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
         alt="pokemon vert"
+        className="card-img"
       />
       <figcaption>Bulbazor</figcaption>
     </figure>

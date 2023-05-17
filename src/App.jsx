6dadfe781663896baddcba35faa804e88e8f8780
Benjamin Laroche <benjamin.laroche28@gmail.com>
2023-05-17 22:43:00 +0200

    first commit bulbizar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import MyTitle from "./components/MyTitle";
+import PokemonCard from "./components/PokemonCard";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <MyTitle />
+      <PokemonCard />
     </div>
   );
 }
